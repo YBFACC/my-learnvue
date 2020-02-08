a9c30cf97f1c00a756dbaa9d284715df9f989657
bingfeng yu <ybf0711@gmail.com>
2020-02-08 23:53:42 +0800

    没有用vue-axios直接用axios

--- src/main.js
+++ src/main.js
@@ -5,11 +5,6 @@ import router from './router'
 import store from './store'
 import './config/rem'
 
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-
-Vue.use(VueAxios, axios)
-
 Vue.config.productionTip = false
 
 new Vue({
