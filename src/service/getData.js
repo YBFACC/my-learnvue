5db01b1018abfab87f574477ae9ec1da789608ae
bingfeng yu <ybf0711@gmail.com>
2020-02-19 21:17:51 +0800

    获取当前所在城市和搜索地址api

--- src/service/getData.js
+++ src/service/getData.js
@@ -17,3 +17,23 @@ export const groupcity = () => axios.get(baseUrl + '/v1/cities?type=group')
  */
 
 export const cityGuess = () => axios.get(baseUrl + '/v1/cities?type=guess')
+
+/**
+ * 获取当前所在城市
+ */
+export const currentcity = number => axios.get(baseUrl + '/v1/cities/' + number)
+
+/**
+ * 获取搜索地址
+ */
+
+export const searchplace = (cityid, value) =>
+  axios.get(
+    baseUrl +
+      '/v1/pois?type=search' +
+      'city_id=' +
+      cityid +
+      '&' +
+      'keyword=' +
+      value
+  )
