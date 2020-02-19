230f99c84a0c7ce562c58edbe45ce357d1a35627
bingfeng yu <ybf0711@gmail.com>
2020-02-19 21:20:02 +0800

    localStorage的使用,city发送请求

--- src/pages/city/city.vue
+++ src/pages/city/city.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="city_container">
-    <head-top head-title="北京" go-back='true'>
+    <head-top head-title="北京" go-back="true">
       <template #changecity>
         <router-link to="/home" class="change_city">切换城市</router-link>
       </template>
@@ -49,7 +49,8 @@
 </template>
 <script>
 import headTop from '@/components/header/head'
-//import { getStore, setStore, removeStore } from '@/config/mUtils'
+import { currentcity, searchplace } from '@/service/getData'
+import { getStore, setStore, removeStore } from '@/config/mUtils'
 
 export default {
   name: 'City',
@@ -65,6 +66,62 @@ export default {
       placeHistory: [], // 历史搜索记录
       historytitle: true, // 默认显示搜索历史头部，点击搜索后隐藏
       placeNone: false // 搜索无结果，显示提示信息
+    }
+  },
+  mounted() {
+    this.cityid = this.$route.params.cityid
+    currentcity(this.cityid).then(res => {
+      this.cityname = res.data.name
+    })
+    this.initData()
+  },
+  methods: {
+    initData() {
+      //获取搜索历史记录
+      if (getStore('placeHistory')) {
+        this.placelist = JSON.parse(getStore('placeHistory'))
+      } else {
+        this.placelist = []
+      }
+    },
+    //发送搜索信息inputVaule
+    postpois() {
+      //输入值不为空时才发送信息
+      if (this.inputVaule) {
+        searchplace(this.cityid, this.inputVaule).then(res => {
+          this.historytitle = false
+          this.placelist = res.data
+          this.placeNone = res.length ? false : true
+        })
+      }
+    },
+    /**
+     * 点击搜索结果进入下一页面时进行判断是否已经有一样的历史记录
+     * 如果没有则新增，如果有则不做重复储存，判断完成后进入下一页
+     */
+    nextpage(index, geohash) {
+      let history = getStore('placeHistory')
+      let choosePlace = this.placelist[index]
+      if (history) {
+        let checkrepeat = false
+        this.placeHistory = JSON.parse(history)
+        this.placeHistory.forEach(item => {
+          if (item.geohash == geohash) {
+            checkrepeat = true
+          }
+        })
+        if (!checkrepeat) {
+          this.placeHistory.push(choosePlace)
+        }
+      } else {
+        this.placeHistory.push(choosePlace)
+      }
+      setStore('placeHistory', this.placeHistory)
+      this.$router.push({ path: '/msite', query: { geohash } })
+    },
+    clearAll() {
+      removeStore('placeHistory')
+      this.initData()
     }
   }
 }
