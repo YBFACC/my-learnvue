8a05b840a420e3945afcf9292805f1f3e7d4f6be
bingfeng yu <ybf0711@gmail.com>
2020-02-20 22:48:15 +0800

    初始化

---
+++ src/pages/login/login.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="loginContainer"></div>
+</template>
+
+<script>
+
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../style/mixin';
+
+.loginContainer {
+  padding-top: 1.95rem;
+  p,
+  span,
+  input {
+    font-family: Helvetica Neue, Tahoma, Arial;
+  }
+}
+.change_login {
+  position: absolute;
+  @include ct;
+  right: 0.75rem;
+  @include sc(0.7rem, #fff);
+}
+
+.loginForm {
+  background-color: #fff;
+  margin-top: 0.6rem;
+  .input_container {
+    display: flex;
+    justify-content: space-between;
+    padding: 0.6rem 0.8rem;
+    border-bottom: 1px solid #f1f1f1;
+    input {
+      @include sc(0.7rem, #666);
+    }
+    button {
+      @include sc(0.65rem, #fff);
+      font-family: Helvetica Neue, Tahoma, Arial;
+      padding: 0.28rem 0.4rem;
+      border: 1px;
+      border-radius: 0.15rem;
+    }
+    .right_phone_number {
+      background-color: #4cd964;
+    }
+  }
+  .phone_number {
+    padding: 0.3rem 0.8rem;
+  }
+  .captcha_code_container {
+    height: 2.2rem;
+    .img_change_img {
+      display: flex;
+      align-items: center;
+      img {
+        @include wh(3.5rem, 1.5rem);
+        margin-right: 0.2rem;
+      }
+      .change_img {
+        display: flex;
+        flex-direction: 'column';
+        flex-wrap: wrap;
+        width: 2rem;
+        justify-content: center;
+        p {
+          @include sc(0.55rem, #666);
+        }
+        p:nth-of-type(2) {
+          color: #3b95e9;
+          margin-top: 0.2rem;
+        }
+      }
+    }
+  }
+}
+.login_tips {
+  @include sc(0.5rem, red);
+  padding: 0.4rem 0.6rem;
+  line-height: 0.5rem;
+  a {
+    color: #3b95e9;
+  }
+}
+.login_container {
+  margin: 0 0.5rem 1rem;
+  @include sc(0.7rem, #fff);
+  background-color: #4cd964;
+  padding: 0.5rem 0;
+  border: 1px;
+  border-radius: 0.15rem;
+  text-align: center;
+}
+.button_switch {
+  background-color: #ccc;
+  display: flex;
+  justify-content: center;
+  @include wh(2rem, 0.7rem);
+  padding: 0 0.2rem;
+  border: 1px;
+  border-radius: 0.5rem;
+  position: relative;
+  .circle_button {
+    transition: all 0.3s;
+    position: absolute;
+    top: -0.2rem;
+    z-index: 1;
+    left: -0.3rem;
+    @include wh(1.2rem, 1.2rem);
+    box-shadow: 0 0.026667rem 0.053333rem 0 rgba(0, 0, 0, 0.1);
+    background-color: #f1f1f1;
+    border-radius: 50%;
+  }
+  .trans_to_right {
+    transform: translateX(1.3rem);
+  }
+  span {
+    @include sc(0.45rem, #fff);
+    transform: translateY(0.05rem);
+    line-height: 0.6rem;
+  }
+  span:nth-of-type(2) {
+    transform: translateY(-0.08rem);
+  }
+}
+.change_to_text {
+  background-color: #4cd964;
+}
+.to_forget {
+  float: right;
+  @include sc(0.6rem, #3b95e9);
+  margin-right: 0.3rem;
+}
+</style>
