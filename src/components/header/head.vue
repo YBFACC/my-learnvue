1cd15b4bfa62201e616c28a21c538ea8df7094ec
bingfeng yu <ybf0711@gmail.com>
2020-02-27 16:38:01 +0800

    抽离了一个svg文件写login页面

--- src/components/header/head.vue
+++ src/components/header/head.vue
@@ -3,21 +3,11 @@
     <slot name="logo"></slot>
     <slot name="search"></slot>
     <section class="head_goback" v-if="goBack" @click="$router.go(-1)">
-      <svg
-        width="100%"
-        height="100%"
-        xmlns="http://www.w3.org/2000/svg"
-        version="1.1"
-      >
-        <polyline
-          points="12,18 4,9 12,0"
-          style="fill:none;stroke:rgb(255,255,255);stroke-width:2"
-        />
-      </svg>
+      <go-back></go-back>
     </section>
-    <div class="head_login">
-      <span class="login_span"></span>
-    </div>
+    <router-link to="/login" class="head_login">
+      <span class="login_span">登录|注册</span>
+    </router-link>
     <section class="title_head ellipsis" v-if="headTitle">
       <span class="title_text">{{ headTitle }}</span>
     </section>
@@ -26,9 +16,13 @@
 </template>
 
 <script>
+import GoBack from '@/components/svg/goback.vue'
 export default {
   name: 'Head',
-  props: ['headTitle', 'goBack']
+  props: ['signinUp', 'headTitle', 'goBack'],
+  components: {
+    GoBack
+  }
 }
 </script>
 <style lang="scss" scoped>
