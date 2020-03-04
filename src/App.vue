831ad9542bb20397a63fd35aab197178056c923e
bingfeng yu <ybf0711@gmail.com>
2020-03-05 00:26:59 +0800

    引入svg文件

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,14 @@
   </div>
 </template>
 
+<script>
+import svgIcon from '@/components/common/svg'
+export default {
+  components: {
+    svgIcon
+  }
+}
+</script>
 <style lang="scss">
 @import './style/common';
 .router-fade-enter-active,
