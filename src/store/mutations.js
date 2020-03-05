db5c05e726aa23755fb0f6db91489ad94205b1f8
bingfeng yu <ybf0711@gmail.com>
2020-03-05 22:35:35 +0800

    GET_USERINFO

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,9 +1,22 @@
-import { RECORD_USERINFO } from './mutation-types'
+import { RECORD_USERINFO,GET_USERINFO } from './mutation-types'
 
 
 export default{
   [RECORD_USERINFO](state,info){
     state.userInfo = info;
 		state.login = true;
+  },
+  [GET_USERINFO](state,info){
+    if (state.userInfo && (state.userInfo.username !== info.username)) {
+			return;
+		};
+		if (!state.login) {
+			return
+		}
+		if (!info.message) {
+			state.userInfo = {...info};
+		} else {
+			state.userInfo = null;
+		}
   }
 }
